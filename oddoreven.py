5d74275145176444de023faa83801df46314df77
ALAN P NATH <68515757+alan-pnath@users.noreply.github.com>
2022-03-15 10:28:32 +0530

    Add files via upload

---
+++ oddoreven.py
@@ -0,0 +1,5 @@
+x=int(input("Enter the number: "))
+if x%2==0:
+    print("the Number is EVEN")
+else:
+    print("the Number is ODD")
---
+++ sumofsqr.py
@@ -0,0 +1,8 @@
+#sum of sqaure of n natural number
+
+x=int(input("enter the limit"))
+sum=0            
+for i in range(0,x+1):
+    sum=sum+(i**2)
+print(sum)
+    
